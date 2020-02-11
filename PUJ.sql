dbcef7efde1903cd06e16df86d95a3a1b636cd9b
Ivan <ivankamsigovski2@gmail.com>
2020-02-11 21:31:40 +0100

    Baza

---
+++ PUJ.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 11, 2020 at 09:29 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `PUJ`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kontakt`
+--
+
+CREATE TABLE `kontakt` (
+  `id` int(11) NOT NULL,
+  `ime` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `prezime` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `Knjiga` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `kontakt`
+--
+
+INSERT INTO `kontakt` (`id`, `ime`, `prezime`, `Knjiga`) VALUES
+(1, 'Ivan', 'Kamšigovski', 'PINOKIO (Carlo Collodi)'),
+(2, 'Robert', 'Bukovac', 'Silmarillion (J.R.R. Tolkien)'),
+(3, 'Stanko', 'Zovko', 'It (Stephen King)'),
+(35, '', '', ''),
+(36, '', '', ''),
+(37, '', '', ''),
+(38, '', '', ''),
+(39, '', '', ''),
+(40, '', '', ''),
+(41, '', '', ''),
+(42, '', '', ''),
+(43, '', '', ''),
+(44, '', '', ''),
+(45, '', '', ''),
+(46, '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `korisnik`
+--
+
+CREATE TABLE `korisnik` (
+  `id` int(11) NOT NULL,
+  `korisnicko_ime` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `lozinka` varchar(128) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `korisnik`
+--
+
+INSERT INTO `korisnik` (`id`, `korisnicko_ime`, `lozinka`) VALUES
+(1, 'Robert', 'roba'),
+(2, 'Ivan', 'ivan'),
+(3, 'Stanko', 'stana');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kontakt`
+--
+ALTER TABLE `kontakt`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `korisnik`
+--
+ALTER TABLE `korisnik`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kontakt`
+--
+ALTER TABLE `kontakt`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT for table `korisnik`
+--
+ALTER TABLE `korisnik`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
